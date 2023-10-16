898ec8120a34031b799118f809744198d17662fa
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-16 20:43:36 +1100

    Implement jokes endpoint to provide a list of jokes

--- src/main/kotlin/com/nit3213/spring/dadjokescentral/controllers/MainController.kt
+++ src/main/kotlin/com/nit3213/spring/dadjokescentral/controllers/MainController.kt
@@ -2,6 +2,7 @@ package com.nit3213.spring.dadjokescentral.controllers
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,4 +14,11 @@ class MainController {
 
     @RequestMapping("/creators") //localhost:8080/api/creators
     fun returnProjectCreators() = "Name 1, Name 2"
+
+    @RequestMapping("/jokes", method = arrayOf(RequestMethod.GET)) //localhost:8080/api/jokes
+    fun returnJokes() = listOf("Why don't scientists trust atoms? Because they make up everything!\n",
+            "What did one plate say to another plate? \"Tonight, dinner's on me!\"\n",
+            "Why did the scarecrow win an award? Because he was outstanding in his field!\n",
+            "How does a penguin build its house? Igloos it together!\n",
+            "Did you hear about the mathematician who’s afraid of negative numbers? He will stop at nothing to avoid them!")
 }
