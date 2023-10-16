321a325f191ebec43e404cc31474168d34f86898
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-16 20:39:16 +1100

    specifying a HTTP method type

--- src/main/kotlin/com/nit3213/spring/dadjokescentral/controllers/ClientDataController.kt
+++ src/main/kotlin/com/nit3213/spring/dadjokescentral/controllers/ClientDataController.kt
@@ -6,18 +6,18 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api")
 class ClientDataController {
 
-    @RequestMapping("/simple/{number}") // {nameOfValueHere} should be same as value -> @PathVariable(required = false) nameOfValueHere: Int
+    @RequestMapping("/simple/{number}", method = arrayOf(RequestMethod.GET)) // {nameOfValueHere} should be same as value -> @PathVariable(required = false) nameOfValueHere: Int
     fun showTheParam(@PathVariable(required = false) number: Int): String {
 
         return "The number you shared is $number"
     }
 
-    @RequestMapping("/query") //localhost:8080/api/query?author=Jacob
+    @RequestMapping("/query", method = arrayOf(RequestMethod.GET)) //localhost:8080/api/query?author=Jacob
     fun respondWithQuery(@RequestParam(required = false, defaultValue = "") author: String): String {
         return "Author's name is $author"
     }
 
-    @RequestMapping("/doublequery") //localhost:8080/api/doublequery?author=DavidFinch&location=Ireland
+    @RequestMapping("/doublequery", method = arrayOf(RequestMethod.GET, RequestMethod.POST)) //localhost:8080/api/doublequery?author=DavidFinch&location=Ireland
     fun respondWithDoubleQuery(@RequestParam(required = false, defaultValue = "") author: String, location: String): String {
         return "Author's name is $author and location is $location"
     }
